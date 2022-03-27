964f6d8e39d26dd60a0ad86423f7d1f163680bce
a-oyi <101929114+a-oyi@users.noreply.github.com>
2022-03-27 20:20:21 +0800

    Delete 配置文件 directory

--- 配置文件/subpage/page2/js/index.js
+++
@@ -1,3 +0,0 @@
-setTimeout(function(){document.getElementById('text').style.animationPlayState = "running";},7000);
-setTimeout(function(){document.getElementById('skin').style.display = "block";},12000);
-setTimeout("window.location.href='../page3/index.html'",14000);
--- 配置文件/subpage/page4/js/index.js
+++
@@ -1,9 +0,0 @@
-setTimeout(function(){
-	document.getElementById('email').style.display = "block";
-	document.getElementById('btn').style.display = "block";
-},10000);
-
-function button(){
-	window.open("../page5/小作文.docx");
-	document.getElementById('content').style.animationPlayState = "running"
-}
